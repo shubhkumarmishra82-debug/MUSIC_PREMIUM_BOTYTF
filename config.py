ab7395578c57913bc51380e47b6931a8730ea2e3
MADARA X CODER <ragini.19854@gmail.com>
2026-08-01 11:34:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 # Limits and IDs
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
-OWNER_ID = int(getenv("OWNER_ID", 8723235165))
+OWNER_ID = int(getenv("OWNER_ID", 8762430892))
 
 # Heroku
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")

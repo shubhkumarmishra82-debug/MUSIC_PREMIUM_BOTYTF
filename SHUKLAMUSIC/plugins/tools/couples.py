b923700701e4c4d2a54c1bd35d27d96517610458
MADARA X CODER <ragini.19854@gmail.com>
2026-08-05 16:13:37 +0530

    Update couples.py

--- SHUKLAMUSIC/plugins/tools/couples.py
+++ SHUKLAMUSIC/plugins/tools/couples.py
@@ -41,8 +41,8 @@ def ap(eid, fb):
 POLICE = [
     [
         InlineKeyboardButton(
-            text="ᯓ꯭×𝓓ⲉⲙⲟ꯭ⲛ-֟፝.𓆪᭄ꪾ𑱟",
-            url="https://t.me/Demon_x_coder_aura",
+            text="𝛅 ᥲ s 𝛖 𝛋 ᴇ ࿐",
+            url="https://t.me/sasuke_qt",
         ),
     ],
 ]

1f0e4e3bf2549901b35c0995f00e27184b8bd8d1
MADARA X CODER <ragini.19854@gmail.com>
2026-08-05 16:21:48 +0530

    Update start.py

--- SHUKLAMUSIC/utils/inline/start.py
+++ SHUKLAMUSIC/utils/inline/start.py
@@ -95,7 +95,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(
-                text=_[𝛅 ᥲ s 𝛖 𝛋 ᴇ ࿐],
+                text=_["S_B_5"],
                 url=f"https://t.me/{_clean_username(config.OWNER_USERNAME)}",
                 style=ButtonStyle.DANGER,
                 icon_custom_emoji_id=_E_CROWN
